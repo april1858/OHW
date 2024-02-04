8ded73486f7b2ff94e771bb7fa4e38ccc6991770
april1858 <april1858@april1858.april1858>
2024-02-04 19:31:18 +0700

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,67 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var sb strings.Builder
+	var buff rune
+	for _, r := range s {
+		if buff == 0 {
+			if unicode.IsDigit(r) {
+				return "", ErrInvalidString
+			}
+			buff = r
+		} else {
+			switch {
+			case unicode.IsDigit(r):
+				if unicode.IsLetter(buff) {
+					str := RepeatLetter(r, buff)
+					sb.WriteString(str)
+					buff = 0
+				} else {
+					str := RepeatControl(r)
+					sb.WriteString(str)
+					buff = 0
+				}
+			case unicode.IsLetter(r):
+				if unicode.IsLetter(buff) {
+					sb.WriteString(string(buff))
+					buff = r
+				} else {
+					sb.WriteString("\n")
+					buff = r
+				}
+			case unicode.IsControl(r):
+				if unicode.IsLetter(buff) {
+					sb.WriteString(string(buff))
+					buff = r
+				} else {
+					sb.WriteString("\n")
+					buff = r
+				}
+			}
+		}
+	}
+	if buff != 0 {
+		sb.WriteString(string(buff))
+	}
+	return sb.String(), nil
+}
+
+func RepeatControl(r rune) string {
+	var b strings.Builder
+	for i := 0; i < int(r-'0'); i++ {
+		b.WriteString("\n")
+	}
+	return b.String()
+}
+
+func RepeatLetter(r, buff rune) string {
+	str := strings.Repeat(string(buff), int(r-'0'))
+	return str
 }
