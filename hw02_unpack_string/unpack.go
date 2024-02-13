7c4ef7ccad6022b9a0949764617c28863b713610
april1858 <april1858@april1858.april1858>
2024-02-13 17:54:07 +0700

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,29 +10,81 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	var sb strings.Builder
-	var buff rune
+	var buff, bf rune
+	var letter string
+	var err error
+	slash := 0
 	for _, r := range s {
-		if buff == 0 {
-			if unicode.IsDigit(r) {
-				return "", ErrInvalidString
+		if r == 92 {
+			slash++
+		}
+		switch {
+		case buff == 0:
+			bf, err := buffE(r)
+			if err != nil {
+				return "", err
 			}
-			buff = r
-		} else {
-			switch {
-			case unicode.IsDigit(r):
-				str := Repeat(r, buff)
-				sb.WriteString(str)
-				buff = 0
-			default:
-				sb.WriteString(string(buff))
-				buff = r
+			buff = bf
+		case unicode.IsDigit(r):
+			letter, bf = receivedD(r, buff, slash)
+			sb.WriteString(letter)
+			buff = bf
+			slash = 0
+		default:
+			letter, bf, err = receivedC(r, buff)
+			if err != nil {
+				return "", err
 			}
+			sb.WriteString(letter)
+			buff = bf
 		}
 	}
 	if buff != 0 {
 		sb.WriteString(string(buff))
 	}
 	return sb.String(), nil
+}
+
+func buffE(r rune) (rune, error) {
+	if unicode.IsDigit(r) {
+		return 0, ErrInvalidString
+	}
+	return r, nil
+}
+
+func receivedD(r, buff rune, sl int) (string, rune) {
+	var bf rune
+	var letter string
+	switch {
+	case buff == 92:
+		switch {
+		case sl == 3:
+			return string(buff), r
+		case sl == 2:
+			letter = Repeat(r, buff)
+			bf = 0
+		default:
+			bf = r
+		}
+
+	default:
+		letter = Repeat(r, buff)
+		bf = 0
+	}
+	return letter, bf
+}
+
+func receivedC(r, buff rune) (string, rune, error) {
+	var letter string
+	switch {
+	case r == 110 && buff == 92:
+		return "", 0, ErrInvalidString
+	case r == 92 && buff == 92:
+		return "", buff, nil
+	default:
+		letter = string(buff)
+	}
+	return letter, r, nil
 }
 
 func Repeat(r, buff rune) string {
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,10 +18,10 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -41,6 +41,86 @@ func TestUnpackInvalidString(t *testing.T) {
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestRepeat(t *testing.T) {
+	type args struct {
+		r    rune
+		buff rune
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "first", args: args{r: 51, buff: 97}, want: "aaa"},
+		{name: "second", args: args{r: 48, buff: 97}, want: ""},
+		{name: "third", args: args{r: 53, buff: 92}, want: `\\\\\`},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Repeat(tt.args.r, tt.args.buff); got != tt.want {
+				t.Errorf("Repeat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buffE(t *testing.T) {
+	type args struct {
+		r rune
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    rune
+		wantErr bool
+	}{
+		{name: "first", args: args{r: 49}, want: 0, wantErr: true},
+		{name: "second", args: args{r: 97}, want: 97, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := buffE(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("buffE() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("buffE() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_receivedD(t *testing.T) {
+	type args struct {
+		r    rune
+		buff rune
+		sl   int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 rune
+	}{
+		{name: "first", args: args{r: 51, buff: 97, sl: 0}, want: "aaa", want1: 0},
+		{name: "second", args: args{r: 51, buff: 92, sl: 1}, want: "", want1: 51},
+		{name: "third", args: args{r: 51, buff: 92, sl: 2}, want: `\\\`, want1: 0},
+		{name: "fourth", args: args{r: 51, buff: 92, sl: 3}, want: `\`, want1: 51},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := receivedD(tt.args.r, tt.args.buff, tt.args.sl)
+			if got != tt.want {
+				t.Errorf("receivedD() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("receivedD() got1 = %v, want %v", got1, tt.want1)
+			}
 		})
 	}
 }
