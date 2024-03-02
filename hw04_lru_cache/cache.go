b2b58778af96998286de732ec3307cb2beaa3d3b
april1858 <april1858@april1858.april1858>
2024-03-02 20:53:17 +0700

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -12,12 +12,16 @@ type Cache interface {
 	Clear()
 }
 
+type CacheItem struct {
+	Key   Key
+	Value interface{}
+}
+
 type lruCache struct {
 	mx       sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
-	elements map[interface{}]Key
 }
 
 func NewCache(capacity int) Cache {
@@ -25,27 +29,25 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
-		elements: make(map[interface{}]Key, capacity),
 	}
 }
 
 func (c *lruCache) Set(key Key, value interface{}) bool {
 	c.mx.Lock()
 	defer c.mx.Unlock()
+	item := &CacheItem{Key: key, Value: value}
 	if i, ok := c.items[key]; ok {
-		i.Value = value
+		i.Value = *item
 		c.queue.MoveToFront(i)
 		c.items[key] = c.queue.Front()
 		return true
 	}
 	if c.queue.Len() >= c.capacity {
 		back := c.queue.Back()
-		delete(c.items, c.elements[back.Value])
-		delete(c.elements, back.Value)
+		delete(c.items, back.Value.(CacheItem).Key)
 		c.queue.Remove(back)
 	}
-	c.elements[value] = key
-	c.items[key] = c.queue.PushFront(value)
+	c.items[key] = c.queue.PushFront(*item)
 	return false
 }
 
@@ -54,7 +56,7 @@ func (c *lruCache) Get(key Key) (interface{}, bool) {
 	defer c.mx.Unlock()
 	if i, ok := c.items[key]; ok {
 		c.queue.MoveToFront(i)
-		return i.Value, true
+		return i.Value.(CacheItem).Value, true
 	}
 	return nil, false
 }
