e4a5d6f97386a9ce4c5c183ed10d4bc67818ad2e
april1858 <april1858@april1858.april1858>
2024-05-15 21:38:24 +0700

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"errors"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -11,5 +18,65 @@ var (
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
+
+	count := 3000
+
+	// create and start new bar
+	bar := pb.StartNew(count)
+
+	var sizeBuf int64
+
+	file, err := os.Open(fromPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fi, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if limit == 0 {
+		limit = fi.Size()
+	}
+	if limit > fi.Size() {
+		limit = fi.Size() - offset
+	}
+	if offset > fi.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	s := io.NewSectionReader(file, offset, limit)
+
+	if offset+limit > fi.Size() {
+		sizeBuf = fi.Size() - offset
+	} else {
+		sizeBuf = limit
+	}
+
+	buf := make([]byte, sizeBuf)
+
+	if _, err := s.Read(buf); err != nil {
+		if err != io.EOF {
+			log.Fatal("err - ", err)
+		}
+	}
+	file.Close()
+
+	fileTo, _ := os.Create(toPath)
+
+	if _, err := io.Copy(fileTo, strings.NewReader(string(buf))); err != nil {
+		log.Fatal(err)
+	}
+	fileTo.Close()
+
+	for i := 0; i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+
+	// finish bar
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
+/*
 func TestCopy(t *testing.T) {
 	// Place your code here.
+}
+*/
+
+func TestCopy(t *testing.T) {
+	f, err := os.CreateTemp("", "temp")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(f.Name())
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{name: "1", args: args{fromPath: "testdata/input.txt", toPath: f.Name(), offset: 0, limit: 0}, wantErr: false},
+		{name: "2", args: args{fromPath: "testdata/input.txt", toPath: f.Name(), offset: 10, limit: 110}, wantErr: false},
+		{name: "3", args: args{fromPath: "testdata/input.txt", toPath: f.Name(), offset: 7000, limit: 0}, wantErr: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit); (err != nil) != tt.wantErr {
+				t.Errorf("Copy() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
