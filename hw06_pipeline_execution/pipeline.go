355f31f9058e6d0e96d99b0a145dad015679b04a
april1858 <april1858@april1858.april1858>
2025-02-02 16:37:32 +0700

    HW06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -13,9 +13,38 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return finish(done, stg3(done, stg2(done, stg1(done,
-		stg0(done, gen(done, in), stages[0]), stages[1]), stages[2]), stages[3]))
+
+	out := make(Bi)
+
+	tasks := gen(done, in)
+
+	go func() {
+		defer close(out)
+		for i := 0; i < len(stages); i++ {
+			tasks = stages[i](broker(done, tasks))
+		}
+		for i := range tasks {
+			out <- i
+		}
+	}()
+	return out
+}
+
+func broker(done In, in In) Out {
+	c := make(Bi)
+	go func() {
+		defer close(c)
+		for i := range in {
+			select {
+			case <-done:
+				return
+			default:
+				c <- i
+			}
+		}
+	}()
+
+	return c
 }
 
 func gen(done In, in In) Out {
@@ -24,107 +53,6 @@ func gen(done In, in In) Out {
 	go func() {
 		defer close(out)
 		for i := range in {
-			select {
-			case <-done:
-				return
-			case out <- i:
-			}
-		}
-	}()
-	return out
-}
-
-func stg0(done In, in In, stage Stage) Out {
-	out := make(Bi)
-	go func() {
-		defer close(out)
-		for i := range stage(in) {
-			select {
-			case <-done:
-				return
-			default:
-			}
-			select {
-			case <-done:
-				return
-			case out <- i:
-			}
-		}
-	}()
-	return out
-}
-
-func stg1(done In, in In, stage Stage) Out {
-	out := make(Bi)
-	go func() {
-		defer close(out)
-		for i := range stage(in) {
-			select {
-			case <-done:
-				return
-			default:
-			}
-			select {
-			case <-done:
-				return
-			case out <- i:
-			}
-		}
-	}()
-	return out
-}
-
-func stg2(done In, in In, stage Stage) Out {
-	out := make(Bi)
-	go func() {
-		defer close(out)
-
-		for i := range stage(in) {
-			select {
-			case <-done:
-				return
-			default:
-			}
-			select {
-			case <-done:
-				return
-			case out <- i:
-			}
-		}
-	}()
-	return out
-}
-
-func stg3(done In, in In, stage Stage) Out {
-	out := make(Bi)
-	go func() {
-		defer close(out)
-		for i := range stage(in) {
-			select {
-			case <-done:
-				return
-			default:
-			}
-			select {
-			case <-done:
-				return
-			case out <- i:
-			}
-		}
-	}()
-	return out
-}
-
-func finish(done In, in In) Out {
-	out := make(Bi)
-	go func() {
-		defer close(out)
-		for i := range in {
-			select {
-			case <-done:
-				return
-			default:
-			}
 			select {
 			case <-done:
 				return
